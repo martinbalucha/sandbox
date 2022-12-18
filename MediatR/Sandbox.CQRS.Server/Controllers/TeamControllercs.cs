5d868602d766cdbcb97d883dcb06a718326941bb
martinbalucha <martin.balucha@outlook.com>
2022-12-18 19:54:35 +0100

    Reworked project structure

---
+++ MediatR/Sandbox.CQRS.Server/Controllers/TeamControllercs.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Sandbox.CQRS.Server.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class TeamControllercs : ControllerBase
+{
+    private readonly IMediator mediator;
+
+	public TeamControllercs(IMediator mediator)
+	{
+		this.mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+	}
+
+	[HttpGet]
+	public async Task<Team> Get([FromQuery] Guid id)
+	{
+		var query = new GetTeamQuery();
+		var result = await mediator.Send(query);
+
+		return Ok(result);
+	}
+}
