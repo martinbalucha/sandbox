9cea506d9924fc4c85e2f45350ddd06a9ae3d612
Martin Balucha <martin.balucha@chyron.com>
2023-05-31 10:56:46 +0200

    Created project and docker compose file

---
+++ RabbitMQ/src/Sandbox.RabbitMQ/MessagingConfiguration/MessageBrokerConfiguration.cs
@@ -0,0 +1,8 @@
+﻿namespace Sandbox.RabbitMQ.MessagingConfiguration;
+
+public class MessageBrokerConfiguration
+{
+    public string Host { get; init; } = string.Empty;
+    public string Username { get; init; } = string.Empty;
+    public string Password { get; init; } = string.Empty;
+}
